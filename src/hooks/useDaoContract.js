9d07c1a5174eec78cd0980d7b8bab5658ba5c3a1
Serdar <serdaremin2003@gmail.com>
2023-11-19 03:29:49 +0300

    gitignore updated

--- src/hooks/useDaoContract.js
+++ src/hooks/useDaoContract.js
@@ -2,6 +2,7 @@ import { useState, useEffect,useMemo } from "react";
 import { ethers } from "ethers";
 import { CONTRACT_ADDRESS } from "../Constant/addresses";
 import { CONTRACT_ABI } from "../Constant/abi";
+import { private_key, alchemy_rpc } from "../Constant/key";
 
 export const useDaoContract = () => {
     const [contract, setContract] = useState(null);
@@ -22,8 +23,8 @@ export const useDaoContract = () => {
 
 export const useServerContract =  () => {
     return useMemo(() =>{
-        const provider = new ethers.providers.JsonRpcProvider("https://polygon-mumbai.g.alchemy.com/v2/GRLIfZjmMsEsNXcIcydNE0S_2w28vKqe");
-        const signer = new ethers.Wallet("af6c58447cdb700dd732f65f924b092240c65ee7c33f702ebe0433b283b58bd6", provider);
+        const provider = new ethers.providers.JsonRpcProvider(alchemy_rpc);
+        const signer = new ethers.Wallet(private_key, provider);
         
         const _contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer); //Tx işlemi yapmayacaksan signer yerine povider yaz.r
         
