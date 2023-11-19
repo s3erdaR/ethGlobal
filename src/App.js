7281c8a1cfa98742712a43094a0343a22490c72b
Serdar <serdaremin2003@gmail.com>
2023-11-19 06:33:30 +0300

    UI bugs fixed.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App1.css';
 import { Form } from './video_player/Form';
 import { Youtube } from './video_player/yt';
 import { useEffect, useState } from 'react';
@@ -12,14 +12,11 @@ function App() {
   const ethers = require("ethers")
 
   const [youtubeLink, setYoutubeLink]= useState(null);
+  const [movieTitle, setMovieTitle]= useState(null);
   
-  const [balance, setBalance] = useState("");
 
   const [account, setAccount] = useState("");
   const [provider, setProvider] = useState("");
-
-  const [lastLink, setLastLink] = useState("");
-
   const daoContract = useDaoContract();
   
   const server_contract = useServerContract();
@@ -40,25 +37,6 @@ function App() {
     console.log(signer);
     
   }
-
-  async function getAccountBalance() {
-    try {
-      // Check if MetaMask is installed and connected
-      if (!window.ethereum || !window.ethereum.isConnected()) {
-        throw new Error('Please install MetaMask and connect to an Ethereum network');
-      }
-
-      const address = account;
-      const balance = await provider.getBalance(address);
-      const formattedBalance = ethers.utils.formatEther(balance);
-      setBalance(formattedBalance);
-      console.log(balance);
-      console.log(`Account balance: ${formattedBalance} ETH`);
-    } catch (error) {
-      console.error('Error occurred while fetching the account balance:', error);
-    }
-  }
-
   const[_movieId, setMovieId] = useState("");
   async function vote() {
     await daoContract?.UpVoteMovie(_movieId);
@@ -101,9 +79,10 @@ function App() {
       const chosen_mov = await server_contract?.getCurrentMovie();
       chosen_movie = movies[chosen_mov];
       setYoutubeLink(movies[chosen_mov].trailer); 
+      setMovieTitle(movies[chosen_mov].title);
       console.log("Chosen Movie:"+movies[chosen_mov]);
 
-      await sleep(30);
+      await sleep(15);
       console.log("Loop finished");
       
     }
@@ -118,35 +97,53 @@ function App() {
     return new Promise((resolve) => setTimeout(resolve,seconds*1000));
   }
 
+
   return (
 
-    <div className="App">
-      <h1>DATV</h1>
-      <div>
+    <div className="App1">
+      <header>
+        <h1>DATV</h1>
         <button onClick={() => {
-          if(account) return;
-          connect();
-          // is_allowed();
-        }}
-        >
-        {account ? "Connected": "Connect"}{" "}
-        </button>
-        <button onClick={getAccountBalance}>Get Balance</button>
+        if(account) return;
+        connect();
+      }}
+      >
+       {account ? "Connected": "Connect Metamask"}{" "}
+      </button>
+      </header>
 
-        <h1>Show Proposal</h1>
-        <h1>Vote</h1>
-        <input placeholder = "Enter The Movie ID"value = {_movieId} onChange={(e) => setMovieId(e.target.value)} /><h1></h1>
-        <button onClick={vote}> VoteUp </button>
-        {/* <h1>Moive Name: {films[_movieId].title}, VoteUp: {_currentVote}</h1> */}
-        <h1>Chosen Movie: {chosen_movie?.title}</h1>
-        <h1>Last video is: {lastLink}</h1>
-        <h1>Total amount is: {balance}</h1>
+      <div className="content-container proposal">
+        <h2>Today's Proposal</h2>
+        <div>
+          <h3>Which movie will we watch today?</h3>
+          <select
+            id="movieSelect"
+            value={_movieId}
+            onChange={(e) => setMovieId(ethers.BigNumber.from(e.target.value))}
+          >
+            <option value="1">Inception</option>
+            <option value="2">The Shawshank Redemption</option>
+            <option value="3">The Dark Knight</option>
+            <option value="4">Pulp Fiction</option>
+            <option value="5">Forrest Gump</option>
+            <option value="6">The Matrix</option>
+            <option value="7">Titanic</option>
+            <option value="8">Avatar</option>
+            <option value="9">The Godfather</option>
+            <option value="10">Schindler\'s List</option>
+          </select>
+          <br />
+          
+          <button onClick={vote}>Submit Vote</button>
+        </div>
       </div>
 
-      <h1>Watch The Film</h1>
-        <br></br>
-        <Youtube youtubeLink={youtubeLink}/>
-        {/* <Youtube youtubeLink={lastLink}/> */}
+      <div className="content-container">
+        <h2>Time to Watch !!!</h2>        
+        <p>Movie: {movieTitle}</p>
+        <Youtube youtubeLink={youtubeLink}></Youtube>
+        <div id="player"></div>
+      </div>
     </div>
   );
 }
